465db6cb7388e50f55b7d0e4a81d7110989e7ce9
lauri-ra <lrautiala+1@gmail.com>
2022-05-23 23:30:32 +0300

    Fixes for Heroku implementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', request => {
     if(request.method === 'POST') {
@@ -10,6 +11,7 @@ morgan.token('body', request => {
 const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+app.use(cors())
 
 let persons = [
     {
@@ -102,7 +104,7 @@ app.post('/api/persons', (requests, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
