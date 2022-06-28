96a3a359950e136ca236b70c2d514755754d917a
lauri-ra <lrautiala+1@gmail.com>
2022-06-28 15:14:00 +0300

    task 3.20

--- models/person.js
+++ models/person.js
@@ -11,13 +11,29 @@ mongoose.connect(url)
         console.log('error connecting to MongoDB:', error.message)
     })
 
+const validator = (pnum) => {
+    if(pnum.length >= 8 && pnum.includes('-')) {
+        const pnumSplit = pnum.split('-')
+
+        if(pnumSplit[0].length > 1 && pnumSplit[0].length < 4) {
+            return true
+        }
+    }
+    return false
+}
+
+const validateNumber = [validator, 'Phone number is in incorrect format']
+
 const personSchema = new mongoose.Schema({
     id: Number,
     name: {
         type: String,
         minlength: 5,
     },
-    number: String
+    number: {
+        type: String,
+        validate: validateNumber,
+    }
 })
 
 personSchema.set('toJSON', {
